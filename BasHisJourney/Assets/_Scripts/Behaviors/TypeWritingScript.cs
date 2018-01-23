9434d610b7c00b380e0b38c1770a9fe7c5b60fba
Bas <bas@warp.industries>
2018-01-23 20:32:51 +0100

    Update23-1-2018

---
+++ BasHisJourney/Assets/_Scripts/Behaviors/TypeWritingScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TypeWritingScript : MonoBehaviour
+{
+    public float Delay = 0.1f;
+    public string[] FullText;
+    public IntroductionManager IntroManager;
+
+    private string fullText;
+    private string currentText;
+
+    // Use this for initialization
+    void Start()
+    {
+        FullText = IntroManager.IntroText;
+
+        StartCoroutine(ShowText());
+    }
+
+    IEnumerator ShowText()
+    {
+        for (int i = 0; i < FullText.Length; i++)
+        {
+            fullText = FullText[i];
+            for (int j = 0; j < fullText.Length; j++)
+            {
+                currentText = fullText.Substring(0, j);
+                this.GetComponent<Text>().text = currentText;
+                yield return new WaitForSeconds(Delay);
+            }
+        }
+    }
+}
--- BasHisJourney/Assets/_Scripts/Managers/IntroductionManager.cs
+++ BasHisJourney/Assets/_Scripts/Managers/IntroductionManager.cs
@@ -11,8 +11,11 @@ public class IntroductionManager : MonoBehaviour
     public Image[] CinematicBars;
     public Image FadeGameIn;
     public string[] IntroductionText, MovementIntroText;
+    public float Delay = 0.1f;
+    public string[] IntroText;
 
-    public string[] _introText;
+    private string _fullText;
+    private string _currentText;
     private int _value;
     private bool _performance, _performance2;
 
@@ -24,21 +27,23 @@ public class IntroductionManager : MonoBehaviour
         _performance = true;
         _performance2 = true;
 
-        _introText = IntroductionText;
+        IntroText = IntroductionText;
 
-        IntroductionTextObject.text = _introText[0];
+        IntroductionTextObject.text = IntroText[0];
     }
 
     void Start()
     {
         FadeGameIn.CrossFadeAlpha(0f, 3f, true);
         StartCoroutine(SetInOrActive(FadeGameIn.gameObject, 3, false));
+        StartCoroutine(ShowText());
+
     }
 
     void Update()
     {
         //Intro with cinematic bars
-        if (_introText.Last() == _introText[_value] && _performance)
+        if (IntroText.Last() == IntroText[_value] && _performance)
         {
             Debug.Log("CheckPerformance");
             NextButton.gameObject.SetActive(false);
@@ -54,7 +59,7 @@ public class IntroductionManager : MonoBehaviour
             _performance = false;
         }
 
-        if (MovementIntroText.Last() == _introText[_value] && _performance2)
+        if (MovementIntroText.Last() == IntroText[_value] && _performance2)
         {
             Debug.Log("CheckPerformance2");
             IntroductionTextObject.CrossFadeAlpha(0,3f,false);
@@ -64,11 +69,29 @@ public class IntroductionManager : MonoBehaviour
         }
     }
 
+    IEnumerator ShowText()
+    {
+        for (int i = 0; i < IntroText.Length; i++)
+        {
+            _fullText = IntroText[i];
+            for (int j = 0; j < _fullText.Length; j++)
+            {
+                IntroText[i] = _fullText.Substring(0, j);
+                yield return new WaitForSeconds(Delay);
+            }
+        }
+    }
+
     public void NextText()
     {
         _value++;
         Debug.Log(_value);
-        IntroductionTextObject.text = _introText[0 + _value];
+        IntroductionTextObject.text = IntroText[0 + _value];
+    }
+
+    public void FieldInOrActiveOnClick(GameObject closeObj)
+    {
+        closeObj.gameObject.SetActive(false);
     }
 
     IEnumerator SetInOrActive(GameObject obj, int time, bool active)
@@ -86,9 +109,11 @@ public class IntroductionManager : MonoBehaviour
     IEnumerator SetStringAfterTime(string[] obj, int time)
     {
         yield return new WaitForSeconds(time);
-        _introText = obj;
-        IntroductionTextObject.text = _introText[0];
+        IntroText = obj;
+        IntroductionTextObject.text = IntroText[0];
         _value = 0;
         NextButton.gameObject.SetActive(true);
     }
+
+
 }
